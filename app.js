e679d6e156447652f919ed42b758cb23c962e8f3
rob <robmathew21@gmail.com>
2024-12-04 16:05:57 -0600

    finished logic, and rotated clock by 180 deg to make it accurate

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const hourHand = document.querySelector('.hourHand');
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds/60) * 360) + 90;
+    // console.log(`seconds: ${seconds}`)
+    const secondsDegrees = ((seconds/60) * 360) + 90 + 180;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes/60) * 360) + 90;
+    // console.log(`minutes: ${minutes}`)
+    const minutesDegrees = ((minutes/60) * 360) + 90 + 180;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hours = now.getHours();
-    const hoursDegrees = ((hours/60) * 360) + 90;
+    // console.log(`hours: ${hours}`)
+    const hoursDegrees = ((hours/12) * 360) + ((minutes/60)*30) + 90 + 180;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
-
 }
 
 setInterval(setDate, 1000);
+
+setDate();
